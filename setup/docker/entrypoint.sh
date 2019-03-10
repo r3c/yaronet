3795a0a65116110167a6da991d58147245ecf256
r3c <r3c@nowhere.com>
2019-03-10 14:37:35 +0100

    [setup.docker] Move Docker-related files to subdirectory.

--- setup/docker/entrypoint.sh
+++ setup/docker/entrypoint.sh
@@ -1,27 +1,26 @@
-#!/bin/bash
+#!/bin/sh -e
 
 # Mount application folder to Apache default location.
 # This avoids creating and configuring a new Apache location.
 if [ -d /var/www/html ]; then
-  cd /
-  rm -rf /var/www/html
+	rm -rf /var/www/html
 fi
-ln -s ${APP_HOME}/src /var/www/html
+
+ln -s "${APP_HOME}/src" /var/www/html
+
+cd "${APP_HOME}"
 
 # Install dependencies
-cd ${APP_HOME}
 chown -R www-data:www-data setup/module src /var/www
 npm install
 su -s setup/configure.sh www-data
 
 # Initialize database
 php setup/docker/initdb.php
-if [ $? -ne "0" ]; then
-  exit 1
-fi
 
 # Start Apache service
 if [ -f /var/run/apache2/apache2.pid ]; then
-  rm /var/run/apache2/apache2.pid
+	rm /var/run/apache2/apache2.pid
 fi
+
 /usr/sbin/apache2 -DFOREGROUND
